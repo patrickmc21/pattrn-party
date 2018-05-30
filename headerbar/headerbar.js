81040f7043291cd55168a27de070c755243e11b0
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-30 14:48:10 -0600

    Add header desktop view

--- headerbar/headerbar.js
+++ headerbar/headerbar.js
@@ -2,14 +2,14 @@ $('.header__nav--open-btn').on('click', toggleMenu);
 $('.header__nav--close-btn').on('click', closeMenu);
 
 function toggleMenu() {
-  if (window.innerWidth <= 500) {
+  if (window.innerWidth <= 600) {
     $('.header__nav--link-wrapper').toggleClass('hidden');
     $('.header').toggleClass('grow');
   }
 }; 
 
 function closeMenu() {
-  if (window.innerWidth <= 500) {
+  if (window.innerWidth <= 600) {
     $('.header__nav--link-wrapper').addClass('hidden');
     $('.header').removeClass('grow');
   }
